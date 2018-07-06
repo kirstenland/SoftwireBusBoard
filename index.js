f5733e6d53ccc13d29483c01314f64e8a7138feb
Jan Marucha <jan.jarucha@softwire.com>
2018-07-06 11:46:56 +0100

    add user input

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const readlineSync = require("readline-sync");
 const getCoords = require("./get-coords");
 const getStops = require("./tfl-api/get-stops");
 
-getCoords("NW5 1TL")
+let postcode = readlineSync.question("Postcode: ")
+
+getCoords(postcode) //NW5 1TL
 .then(getStops)
 .then(stops => stops.splice(0,2))
 .then(stops => Promise.all(stops.map(stop=>stop.getArrivals())))
