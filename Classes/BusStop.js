e9a72874811b459f40926fa3dfbe1cca4f0c6b47
Jan Marucha <jan.jarucha@softwire.com>
2018-07-06 11:33:40 +0100

    display buses for two closest station

--- Classes/BusStop.js
+++ Classes/BusStop.js
@@ -1,3 +1,6 @@
+const chalk = require("chalk");
+const getBuses = require("../tfl-api/get-buses");
+
 class BusStop {
     constructor(api_response) {
         this.id = api_response.naptanId,
@@ -5,6 +8,17 @@ class BusStop {
     }
     toString() {
         return `STATION ${this.commonName} (${this.id})`
+    }
+    getArrivals() {
+        return getBuses(this)
+        .then(buses=>this.buses = buses.sort((a,b)=> a.timeToStation - b.timeToStation).splice(0,5))
+        .then(()=>this);
+    }
+    prettyPrint() {
+        console.log(chalk.green(this.toString()+":"));
+        for (let bus of this.buses) {
+            console.log("  "+bus.toString());
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-const request = require("request-promise-native");
 const readlineSync = require("readline-sync");
 
 const getCoords = require("./get-coords");
-const getBuses = require("./tfl-api/get-buses");
-
 const getStops = require("./tfl-api/get-stops");
 
-getCoords("NW5 1TL").then(getStops).then((stops) => {
-    stops.forEach((stop) => {
-        console.log(stop.toString());
-        getBuses(stop).then(console.log);
-    } )
-});
-
-//const stopCode = readlineSync.question("Enter a stop code:");
-/*
-request(`https://api.tfl.gov.uk/StopPoint/${stopCode}/arrivals`, function(error, response, body) {
-    let buses = JSON.parse(body);
-    buses.sort(function(bus1, bus2) {
-        return bus1.timeToStation - bus2.timeToStation;
-    })
-    buses.splice(0,5).forEach(bus =>
-        console.log(`${bus.lineName} ${(bus.timeToStation/60).toFixed(0)} minutes away`)
-    );
-})
-*/
+getCoords("NW5 1TL")
+.then(getStops)
+.then(stops => stops.splice(0,2))
+.then(stops => Promise.all(stops.map(stop=>stop.getArrivals())))
+.then(stops => stops.forEach((stop) => stop.prettyPrint()));
